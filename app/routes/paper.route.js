72cfe9533bd59a019a2f2f7925fdae56db0e6074
Anuruddha Thennakoon <anuruddha.thennakoon@gmail.com>
2017-12-08 11:48:47 +0530

    add route to paper

--- app/routes/paper.route.js
+++ app/routes/paper.route.js
@@ -7,8 +7,14 @@ router.route('/')
     .post(paper.insertPaper)
 
     .get(paper.findPaper)
-    
+
 router.route('/:id')
+
+    .put(paper.updatePaper)
+
+    .delete(paper.deletePaper)
+
+router.route('/:idb')
 
     .put(paper.updatePaper)
 
